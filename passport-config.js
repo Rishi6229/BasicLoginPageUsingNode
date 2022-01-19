edf77dbd7d7f00d4f17e193f1bfb149b43187d8d
Rishi Jha <rk6180442@gmail.com>
2022-01-19 09:18:41 +0530

    first commit

---
+++ passport-config.js
@@ -0,0 +1,35 @@
+const LocalStrategy = require('passport-local').Strategy
+const bcrypt = require('bcrypt')
+
+ function initialize(passport ,getUserByEmail){
+  
+  const authenitcateUser = async (email , password , done)=>{
+     const user = getUserByEmail(email)
+     if(user == null){
+         return done(null , false, {message: 'No user with that e-mail'})
+     } 
+  }
+  
+   try{
+       if(await bcrypt.compare(password,user.password)){
+           return done(null , user)
+       }else{
+           return done(null , false ,{message: 'Password incorrect '})
+       }
+ 
+   } 
+   catch(e){
+       return done(e)
+
+   }
+
+
+
+ passport.use(new LocalStrategy({usernameField: 'email'},
+  authenitcateUser))
+  passport.serializeUser((user , done) =>{  })
+  passport.deserializeUser((id , done)=>{})
+  
+}
+
+module.exports = initialize
